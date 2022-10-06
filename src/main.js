01783af90e6ded148445be21f9d7f9e99dc88b6c
chenwj <1765189937@qq.com>
2022-10-06 21:06:49 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import store from './store'
 // 加载全局变量样式
 import './styles/index.less'
 
+// 导入vant组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
